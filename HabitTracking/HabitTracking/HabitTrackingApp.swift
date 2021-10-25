09de6a239a3491bee6c1782fe1bedb4c02f93c35
Mariana Yamamoto <myamamot@lifescan.com>
2021-10-25 12:47:38 -0400

    init project

---
+++ HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Mariana Yamamoto on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
